f9fb7fd6e1d6355c449a8d84fa8e3ed370292c17
mortezamohiuodin <mortezamohiuodin@gmail.com>
2024-12-22 03:04:18 +0330

    add poweroups and coin animation

--- powerup.gd
+++ powerup.gd
@@ -3,10 +3,14 @@ extends Area2D
 var screensize = Vector2.ZERO
 
 func pickup():
-	#$CollisionShape2d.set_deferred("disabled", true)
+	$CollisionShape2d.set_deferred("disabled", true)
 	var tw = create_tween().set_parallel().set_trans(Tween.TRANS_QUAD)
 	tw.tween_property(self, "scale", scale * 3, 0.3)
 	tw.tween_property(self, "modulate:a", 0.0, 0.3)
 	await tw.finished
 	queue_free()
 	
+
+
+func _on_lifetime_timout():
+	queue_free()
